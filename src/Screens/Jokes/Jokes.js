8f986bc92935f3c01c04cbc7573739d5b18f11d1
HarshilAmin001 <79329690+HarshilAmin001@users.noreply.github.com>
2022-01-23 00:38:08 -0500

    drawer added

---
+++ src/Screens/Jokes/Jokes.js
@@ -0,0 +1,15 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import styles from './styles';
+// create a component
+const Jokes = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Jokes</Text>
+        </View>
+    );
+};
+
+//make this component available to the app
+export default Jokes;
